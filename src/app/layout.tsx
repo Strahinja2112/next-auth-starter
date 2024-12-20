e7bc022fd0befaea8a0e686af896a9bece28fc92
strahinja2112 <pavicevicstrahinja99@gmail.com>
2024-12-20 13:52:04 +0100

    new little update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,39 +14,41 @@ import { languageTag } from '@/paraglide/runtime.js';
 
 import '@/styles/globals.css';
 
-export const generateMetadata = (): Metadata => ({
-  metadataBase: new URL(siteConfig.url()),
-  title: {
-    default: siteConfig.title(),
-    template: `%s | ${siteConfig.title()}`,
-  },
-  description: siteConfig.description(),
-  keywords: siteConfig.keywords(),
-  robots: { index: true, follow: true },
-  icons: {
-    icon: '/favicon/favicon.ico',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
-  },
-  verification: {
-    google: siteConfig.googleSiteVerificationId(),
-  },
-  openGraph: {
-    url: siteConfig.url(),
-    title: siteConfig.title(),
+export function generateMetadata() {
+  return {
+    metadataBase: new URL(siteConfig.url()),
+    title: {
+      default: siteConfig.title(),
+      template: `%s | ${siteConfig.title()}`,
+    },
     description: siteConfig.description(),
-    siteName: siteConfig.title(),
-    images: '/opengraph-image.png',
-    type: 'website',
-    locale: languageTag(),
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.title(),
-    description: siteConfig.description(),
-    images: '/opengraph-image.png',
-  },
-});
+    keywords: siteConfig.keywords(),
+    robots: { index: true, follow: true },
+    icons: {
+      icon: '/favicon/favicon.ico',
+      shortcut: '/favicon/favicon-16x16.png',
+      apple: '/favicon/apple-touch-icon.png',
+    },
+    verification: {
+      google: siteConfig.googleSiteVerificationId(),
+    },
+    openGraph: {
+      url: siteConfig.url(),
+      title: siteConfig.title(),
+      description: siteConfig.description(),
+      siteName: siteConfig.title(),
+      images: '/opengraph-image.png',
+      type: 'website',
+      locale: languageTag(),
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: siteConfig.title(),
+      description: siteConfig.description(),
+      images: '/opengraph-image.png',
+    },
+  };
+}
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import { HeroForm } from '@/components/form';
 import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
 import * as m from '@/paraglide/messages';
+import { auth } from './api/auth/[...nextauth]/auth-options';
 
 export default async function Page() {
-  const result = await prisma?.user.findMany();
+  const result = await prisma?.user.findFirst();
+
+  const user = await auth();
 
   return (
     <section className="container mt-10 flex flex-col items-center gap-3 md:absolute md:left-1/2 md:top-1/2 md:mt-0 md:-translate-x-1/2 md:-translate-y-1/2">
-      <pre>{JSON.stringify(result, null, 2)}</pre>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
 
       <h1 className="mb-1 font-mono text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
         {m.nextjs_starter_template_headline()}
